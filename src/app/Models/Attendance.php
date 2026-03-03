ce28a81ccd4b46558c4d0700a8cbabe33883b0fd
mako <immako.236630@gmail.com>
2026-03-03 13:38:00 +0900

    テーブル設計・実行

---
+++ src/app/Models/Attendance.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'punched_in_at',
+        'punched_out_at',
+        'requested_in_at',
+        'requested_out_at',
+        'note',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rests()
+    {
+        return $this->hasMany(Rest::class);
+    }
+}
