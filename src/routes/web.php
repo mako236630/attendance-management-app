37d88e477c9b6a36045a5c59cd6f68df1fc19d08
mako <immako.236630@gmail.com>
2026-03-10 13:04:12 +0900

    管理者勤怠一覧・管理者勤怠詳細画面設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\AttendanceListController;
 use App\Http\Controllers\RquestlistController;
+use App\Http\Controllers\AdminattendancelistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,12 +16,26 @@ use App\Http\Controllers\RquestlistController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware('auth')->group(function () {
+
+Route::get('/admin/login', function () {
+    return view('auth.adminlogin');
+})->name('admin.login');
+Route::post('/admin/login', [Laravel\Fortify\Http\Controllers\AuthenticatedSessionController::class, 'store']);
+
+Route::middleware(['auth', 'Admin'])->group(function () {
+    Route::get('/admin/attendance/list', [AdminattendancelistController::class, 'index'])->name('admin.list');
+    Route::get('/admin/attendance/{id}', [AdminattendancelistController::class, 'show'])->name('admin.show');
+    Route::post('/admin/attendance/{id}', [AdminattendancelistController::class, 'updateRequest'])->name("admin.update");
+});
+
+
+
+Route::middleware(['auth', 'RejectAdmin'])->group(function () {
     
 Route::get("/attendance", [AttendanceController::class, "index"])->name("attendance.index");
 Route::post("/attendance", [AttendanceController::class, "store"])->name("attendance.store");
 Route::get("attendance/list", [AttendanceListController::class, "index"])->name("attendance.list");
 Route::get("/attendance/detail/{id}", [AttendanceListController::class, "show"])->name("attendance.show");
-});
 Route::post("/attendance/detail/{id}", [AttendanceListController::class, "updateRequest"])->name("attendance.update");
 Route::get("/stamp_correction_request/list", [RquestlistController::class, "index"])->name("requestlist.index");
+});
